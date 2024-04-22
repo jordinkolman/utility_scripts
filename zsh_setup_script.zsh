165fc7483200ef643c710b5c251e7fd7f2305558
Jordin Kolman <jordinkolman@gmail.com>
2024-04-22 17:11:23 -0500

    bug fixes

--- zsh_setup_script.zsh
+++ zsh_setup_script.zsh
@@ -9,7 +9,7 @@ git config --global user.name "Jordin Kolman"
 git config --global init.defaultBranch main
 
 echo "Installing Pyenv"
-curl https://penv.run | zsh
+curl https://pyenv.run | zsh
 
 echo "Installing Python"
 pyenv install --quiet 3.12.3 3.11.9
@@ -17,7 +17,7 @@ pyenv global 3.12.3
 
 echo "Installing VSCode"
 sudo apt-get install wget gpg
-wget -q0- https://packages.microsoft.com/keys/microsoft.asc | gpg --darmor > packages.microsoft.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
@@ -26,7 +26,9 @@ sudo apt install code
 
 echo "Installing NeoVim"
 sudo apt-get install neovim
+y
 sudo apt-get install python3-neovim
+y
 
 echo "Installing oh-my-zsh"
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
@@ -42,6 +44,7 @@ sudo apt-get install fzf
 
 echo "Installing xclip"
 sudo apt-get install xclip
+y
 
 echo "Generating SSH Key"
 ssh-keygen -t ed25519 -C "jordinkolman@gmail.com"
